97f57315233094cd349bc0b156af5564b252419e
Demetris Hoplaros <dhoplaros@gmail.com>
2020-04-24 11:22:06 +0300

    Almost there

--- turkish_delight/src/turkish_delight.cr
+++ turkish_delight/src/turkish_delight.cr
@@ -1,13 +1,49 @@
 require "kemal"
 require "uuid"
 require "uuid/json"
-
-# require "./lib/attack"
+require "http/client"
 
 logging false
 
 lib CONSTANTS
   STATUS_RESPONSE = {"status": "ok"}.to_json
+  REFEREE_URL = "enqfc8y2t9fo.x.pipedream.net"
+  REFEREE_WON_URL = REFEREE_URL + "/won"
+
+  OPPONENT_URL = "enqfc8y2t9fo.x.pipedream.net"
+  OPPONENT_STATUS_URL = OPPONENT_URL + "/status"
+  OPPONENT_JAB_PATH = "/jab"
+  OPPONENT_CROSS_PATH = "/cross"
+  OPPONENT_HOOK_PATH = "/hook"
+  OPPONENT_UPPERCUT_PATH = "/uppercut"
+
+  OPPONENT_HTTP_CLIENT = HTTP::Client.new OPPONENT_URL
+end
+
+def attack(path : String)
+  spawn do
+    CONSTANTS::OPPONENT_HTTP_CLIENT.get(path)
+  end
+end
+
+def attack_jab
+  attack CONSTANTS::OPPONENT_JAB_PATH
+end
+
+def attack_cross
+  attack CONSTANTS::OPPONENT_CROSS_PATH
+end
+
+def attack_hook
+  attack CONSTANTS::OPPONENT_HOOK_PATH
+end
+
+def attack_uppercut
+  attack CONSTANTS::OPPONENT_UPPERCUT_PATH
+end
+
+def notify_referee_won
+  HTTP::Client.post(CONSTANTS::REFEREE_WON_URL, body: { name: "Turkish Delight", date: Time.new }.to_json)
 end
 
 def fibonacci(fib_number : Int32)
@@ -22,7 +58,7 @@ def attack_response(fib_number : Int32)
 end
 
 def connection_is_ok : Bool
-  true
+  HTTP::Client.get(CONSTANTS::REFEREE_URL).status_code == 200 && HTTP::Client.get(CONSTANTS::OPPONENT_STATUS_URL).status_code == 200
 end
 
 get "/status" do |env|
@@ -32,10 +68,11 @@ end
 
 get "/test" do |env|
   env.response.content_type = "application/json"
+  result = CONSTANTS::STATUS_RESPONSE
 
-  # return CONSTANTS::STATUS_RESPONSE if true
+  result = { "status": "not connected" }.to_json unless connection_is_ok
 
-  { "status": "not connected" }.to_json
+  result
 end
 
 get "/combat" do |env|
@@ -45,35 +82,35 @@ end
 
 get "/jab" do |env|
   env.response.content_type = "application/json"
-  # TurkishAttack.attack_jab
-  # attack_jab
+  attack_jab
+  attack_jab
 
   attack_response(2)
 end
 
 get "/cross" do |env|
   env.response.content_type = "application/json"
-  # attack_jab
-  # attack_jab
-  # attack_cross
+  attack_jab
+  attack_jab
+  attack_cross
 
   attack_response(4)
 end
 
 get "/hook" do |env|
   env.response.content_type = "application/json"
-  # attack_hook
-  # attack_hook
-  # attack_uppercut
+  attack_hook
+  attack_hook
+  attack_uppercut
 
   attack_response(8)
 end
 
 get "/uppercut" do |env|
   env.response.content_type = "application/json"
-  # attack_cross
-  # attack_hook
-  # attack_uppercut
+  attack_cross
+  attack_hook
+  attack_uppercut
 
   attack_response(16)
 end
