ee1fe84e47790daeaa09ab167eea9902f2d5de47
Angelos Pillos <angelospillos@gmail.com>
2020-04-25 14:22:57 +0300

    Create setup.sh

---
+++ sollip/setup.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get install curl software-properties-common -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt-get install -y nodejs
+node -v
+npm -
+sudo apt install git
+curl -O https://raw.githubusercontent.com/angelospillos/microwars/master/sollip/package.json
+curl -O https://raw.githubusercontent.com/angelospillos/microwars/master/sollip/sollipV4.js
+curl -O https://raw.githubusercontent.com/angelospillos/microwars/master/sollip/sollipV5.js
+sudo apt install -y git
+git --version
+npm install
+node sollipV5.js
